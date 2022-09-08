cdce744f32b432c687607deaf541dfec46a19ec4
obinneji <obinnejic@gmail.com>
2022-09-08 15:53:47 -0700

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf(stderr, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
