fd69b71dfc8e66b6a2714461ae4f2605e5f7059f
obinneji <obinnejic@gmail.com>
2022-09-08 15:20:34 -0700

    advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return 1 after printing  the function
+ */
+int main(void)
+{
+	printf("and that peice of art is useful \" - Dora Korpar, 2015-10-19");
+	return (1);
+}
