db8c0da1db826168e1645c912b2e8517ad7e8016
obinneji <obinnejic@gmail.com>
2022-09-08 15:42:28 -0700

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fwrite("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
