bcb0d17d100c9f2ca2119f55aaa7d621432864f7
obinneji <obinnejic@gmail.com>
2022-09-08 14:19:01 -0700

    function description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing the function
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
