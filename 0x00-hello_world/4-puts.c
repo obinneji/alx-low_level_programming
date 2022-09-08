e2f08f222b398ee3ae128bdfb96c0ef9955761e3
obinneji <obinnejic@gmail.com>
2022-09-08 14:01:35 -0700

    function description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-/*
- * main - puts "programming is like building a multilingual puzzle"
- * Return - 0
- */ 
 	char string[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
