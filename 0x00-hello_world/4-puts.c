ba8363da10a7b5c6cd397b024c72546677bbd557
obinneji <obinnejic@gmail.com>
2022-09-08 06:12:26 -0700

    first programm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int main(){
-	char string[] = "Programming is like building a multilingual puzzle";
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
 	puts(string);
 	return 0;
 }
