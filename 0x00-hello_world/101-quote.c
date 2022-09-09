825dd5f2914bafc3363bc323960a8f480122bead
obinneji <obinnejic@gmail.com>
2022-09-09 04:17:44 -0700

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
 #include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+
 /**
  * main - entry point
  *
@@ -8,5 +10,6 @@
 int main(void)
 {
 	write(1, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	system("rm barhamounafissa > sorti 2>&1");
+	exit(1);
 }
