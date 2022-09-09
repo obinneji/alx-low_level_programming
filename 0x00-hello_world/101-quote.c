cae67ea521c662a7140d81d6e1ba096a6a69158d
obinneji <obinnejic@gmail.com>
2022-09-08 21:19:00 -0700

    solution

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include<stdlib.h>
+#include<stdio.h>
 #include<unistd.h>
 /**
  * main - entry point
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-	write(1,"and that peice of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(1, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
