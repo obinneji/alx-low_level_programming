44542bb1583befd4684804a26ee61d978871dd9f
obinneji <obinnejic@gmail.com>
2022-09-10 14:25:59 -0700

    advanced task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,23 +16,23 @@ int main(void)
 		d = 0;
 		while (d < 10)
 		{
-		
+
 			c = 0;
 			while (c < 10)
 			{
-				if (c != d && d !=e && e < d && d < c)
+				if (c != d && d != e && e < d && d < c)
 				{
 					putchar('0' + e);
 					putchar('0' + d);
 					putchar('0' + c);
 
-					if (c + d + e != 9 + 8+ 7)
+					if (c + d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 			}
-				
+
 				c++;
 
 			}
