72f6224bf770a465ade5a2e8836518572da9f645
obinneji <obinnejic@gmail.com>
2022-09-10 12:46:18 -0700

    task solution

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 /*
 * main - main block
-* Description: print all  alphabets in order from a.
+* Description: print all  alphabets in lower case followed by a new line.
 * Return: 0
 */
 int main(void)
