f085360c15f33fac81a6fc56e908616898722816
obinneji <obinnejic@gmail.com>
2022-09-10 13:14:57 -0700

    task solution

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 /*
 * main - main block
-* Description: Get a random number and check it digits,  compare with 5
+* Description: Get a random number and check it digits, compare with 5
 * Return: 0
 */
 int main(void)
