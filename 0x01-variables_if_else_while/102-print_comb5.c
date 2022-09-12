97c5ae85c9cc98f1c5e9fc94ff21d83e6053f823
Obinneji Chibuzor Francis <87784578+obinneji@users.noreply.github.com>
2022-09-12 04:38:30 -0700

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - a programm that print combination of all two digit.
-* 
-* Return: Always 0 (Success)
-*/
+ * main - A program that prints combination of all two digit numbers 0-99.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
- 	int fTens;
+	int fTens;
 	int fOnes;
 	int sTens;
 	int sOnes;
@@ -18,30 +18,28 @@ int main(void)
 		fTens = num / 10;
 		fOnes = num % 10;
 
-		for(num2 = 0; num2 <= 99; num++)
+		for (num2 = 0; num2 <= 99; num2++)
 		{
 			sTens = num2 / 10;
 			sOnes = num2 % 10;
-			
-			if(fTens < sTens || (fTens == sTens && fOnes < sOnes))
+
+			if (fTens < sTens || (fTens == sTens && fOnes < sOnes))
 			{
 				putchar(fTens + '0');
 				putchar(fOnes + '0');
 				putchar(' ');
 				putchar(sTens + '0');
-				putchar(fOnes + '0');
+				putchar(sOnes + '0');
 
 					if (!(fTens == 9 && fOnes == 8))
 					{
-						
 						putchar(',');
 						putchar(' ');
 					}
-			}
-		}	
+				}
+		}
 	}
-	
-	putchar('\n');
+		putchar('\n');
+
 	return (0);
 }
-
