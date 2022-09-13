5b9c3156a507ee9e7870bd2a9067c71b9833c1ea
obinneji <obinnejic@gmail.com>
2022-09-13 12:15:57 -0700

    task 5

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include "main.h"
+/**
+* _abs - prints absolute values of intergers
+* Description: the function prints out 
+* the absolute value of a number
+* Return: absolute value
+*/
+
+int _abs(int c)
+{
+	return(abs(c));
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+* print_last_digit - prints last digit of a numbers
+* @num: the required input
+* Description: On function call it print last number 
+* in the argurment
+* Return: last digit
+*/
+
+int print_last_digit(int num)
+{
+	return (num % 10)
+}
