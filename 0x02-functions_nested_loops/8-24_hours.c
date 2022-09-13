6397c48f8ba55012d8d1c93c9bbb59e397c415f1
obinneji <obinnejic@gmail.com>
2022-09-13 12:40:44 -0700

    task 5

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* jack_bauer - print every minute of the day
+*
+* Return: Always 0
+*/
+
+void jack_bauer()
+{
+	int hours = 0;
+	int minutes = 0;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			printf("%02d:%0d\n", hours, minutes);
+			minutes++;
+		}
+		hours++;
+		minutes = 0;
+	}
+}
+
