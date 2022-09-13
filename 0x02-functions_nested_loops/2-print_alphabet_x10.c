b2cc8726655e04239c481ba4917c09346e663bd4
obinneji <obinnejic@gmail.com>
2022-09-13 11:16:54 -0700

    task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print all alphabet in lower case
+* Description: prints the result 10 times.
+*/
+
+void  print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		char c = 'a';
+		
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef _MAIN_H_
-#define _MATH_H_
+#ifndef _MAIN_H_INCLUDED
+#define _MATH_H_INCLUDED
 
-int _putchar(char c)
-void print_alphabet(void)
-
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
