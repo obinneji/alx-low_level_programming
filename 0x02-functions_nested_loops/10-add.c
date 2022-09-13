4a98565dd22ec749ed53ffc4cdf1875edc1a03d8
obinneji <obinnejic@gmail.com>
2022-09-13 12:54:03 -0700

    task 5

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - add two numbers
+* @num1: first number
+* @num2: secord number
+* Description: The function adds the two numbers
+* Return: sum of num1 and num2
+*/
+
+int add(int num1, int num2)
+{
+	int result;
+	result = num1 + num2;
+	return (result);
+}
