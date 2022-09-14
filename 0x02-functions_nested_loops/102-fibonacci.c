314d4fd015a84c3898239701ea1c6eb56b321760
obinneji <obinnejic@gmail.com>
2022-09-14 06:08:54 -0700

    advanced task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	long int fib1 = 0, fib2 = 1, fib3, num = 50, count = 0;
+	long int fib1 = 1, fib2 = 2, fib3, num = 50, count = 0;
 
 	while (count <= num)
 	{
@@ -17,7 +17,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%ld, " , fib1);
+			printf("%ld, ", fib1);
 		}
 
 		fib3 = fib1 + fib2;
