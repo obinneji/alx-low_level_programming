6f07282988a980b5c2fe669193db38b5a846ad03
obinneji <obinnejic@gmail.com>
2022-09-14 06:58:53 -0700

    advanced task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	int i;
 	unsigned long int fib1 = 1, fib2 = 2, fib3, sum = 0;
 
-	for (i = 1; 1 <= 33; ++i)
+	for (i = 1; i <= 33; ++i)
 	{
 		if (fib1 < 4000000 && (fib1 % 2) == 0)
 		{
@@ -20,7 +20,7 @@ int main(void)
 		fib1 = fib2;
 		fib2 = fib3;
 	}
-	
+
 	printf("%lu\n", sum);
 
 	return (0);
