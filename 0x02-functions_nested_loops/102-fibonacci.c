8a0bf696fbdf1b489fc09f932a624c8cfab8a176
obinneji <obinnejic@gmail.com>
2022-09-14 05:40:04 -0700

    advanced task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+/**
+* main - print first 50 fibonacci number
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	int fib1 = 0, fib2 = 1, fib3, num = 50, count = 0;
+	
+	while (num < count)
+	{
+	 	fib3 = fib1 + fib2;
+		count++;
+		printf("%d, ", fib3);
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+	
+	return (0);
+}
