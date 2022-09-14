97f3af631f44ef1190c6f96c7e4ea45c16cb0a3f
obinneji <obinnejic@gmail.com>
2022-09-14 06:53:44 -0700

    advanced task

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+* main - print the sum of even numbers in a fibonnacci sequece
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	unsigned long int fib1 = 1, fib2 = 2, fib3, sum = 0;
+
+	for (i = 1; 1 <= 33; ++i)
+	{
+		if (fib1 < 4000000 && (fib1 % 2) == 0)
+		{
+			sum = sum + fib1;
+		}
+		fib3 = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib3;
+	}
+	
+	printf("%lu\n", sum);
+
+	return (0);
+}
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,36 @@
 
 int main(void)
 {
-	unsigned long int i, fib1 = 1, fib2 = 3, fib3, num = 98;
+	unsigned long int i, j, k, j1, j2, k1, k2;
+
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
 	
-	for (i = 1; i <= num; ++i)
+	for (i = 1; i < 91; i++)
 	{
-		if (fib1 != 83621143489848422977)
-		{
-			printf("%u, ", fib1);
-		}
-		else
-		{
-			printf("%u\n", fib1);
-		}
-		fib3 = fib1 + fib2;
-		fib1 = fib2
-		fib2 = fib3
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++1)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
 	
 	return (0);
 }
