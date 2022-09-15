ce029fa05d54221d090752b173fea3a2e0562e84
obinneji <obinnejic@gmail.com>
2022-09-15 10:12:12 -0700

    task 0 - task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,9 @@ void more_numbers(void)
 {
 	int i1, i2;
 
-	for (i1 = 0; i1 <= 9; i1++)
+	for (i1 = 48; i1 <= 57; i1++)
 	{
-		for (i2 = 0 ; i2 <= 14; i2++)
+		for (i2 = 48 ; i2 <= 4952; i2++)
 		{
 			_putchar(i2);
 		}
