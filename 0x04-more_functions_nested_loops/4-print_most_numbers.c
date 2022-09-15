cf36916541d2609109d1f9cb6a0f902a27893c8c
obinneji <obinnejic@gmail.com>
2022-09-15 10:40:13 -0700

    task 0 - task5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,12 +13,11 @@ void print_most_numbers(void)
 	i = 0;
 	while (i < 10)
 	{
-		if (i == 2 || i == 4)
+		if (i != 2 && i != 4)
 		{
-			i++;
-			continue;
+			_putchar('0' + i);
 		}
-		_putchar(i);
+
 		i++;
 	}
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,22 @@
 
 void more_numbers(void)
 {
-	int i1, i2;
+	int i1;
+	char i2, c;
 
-	for (i1 = 48; i1 <= 57; i1++)
+	for (i1 = 0; i1 < 10; i1++)
 	{
-		for (i2 = 48 ; i2 <= 4952; i2++)
+		for (i2 = 0; i2 <= 14; i2++)
 		{
-			_putchar(i2);
+			c = i2;
+			if (i2 > 9)
+			{
+				_putchar('1');
+				c = i2 % 10;
+			}
+
+			_putchar('0' + c);
 		}
-		_putchar('\n');
 	}
 
 	_putchar('\n');
