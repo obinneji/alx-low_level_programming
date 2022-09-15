55e497726734a48be96427a5bee9f5547c4ca139
obinneji <obinnejic@gmail.com>
2022-09-15 11:02:42 -0700

    task 0 - task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print-line - print a line
+*
+* Return: void
+*/
+
+void print-line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+		
+		_putchar('\n')
+	}
+}
