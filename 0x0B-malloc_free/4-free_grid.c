2553bfb9581f9fa55881591688fe0d4c169e866d
obinneji <obinnejic@gmail.com>
2022-10-03 14:43:38 -0700

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+* free_grid - free a two dimwntioanl array
+* @grid: the grid we will free up
+* @height: the number of rows
+* Return: Nothig
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
