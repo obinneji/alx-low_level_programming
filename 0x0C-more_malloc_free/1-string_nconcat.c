f27ff67c3217975d8c05642d6c6d83319cf70029
Obinneji Chibuzor Francis <87784578+obinneji@users.noreply.github.com>
2022-10-08 00:27:41 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,62 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * *string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: limit of s2
- * Return: pointer to new space in memory or null
- **/
+* _strlen - function that returns length or string
+* @str: string to return length
+* Return: Int (length)
+*
+*/
+
+int _strlen(char *str)
+{
+int count;
+
+for (count = 0; str[count] != '\0';)
+count++;
+return (count);
+}
+
+/**
+* string_nconcat - a function that concatenates two strings.
+* @s1: String 1 to concatenate
+* @s2: String 2 to concatenate
+* @n: he first n bytes of s2
+* Return: Pointer | NULL
+*/
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *strDup;
-	int i;
-	unsigned int j;
+int count, count1;
+int sign = n;
+char *ptr;
+int len1, len2;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	strDup = malloc(sizeof(char) * (i + n + 1));
-	if (strDup == NULL)
-		return (NULL);
-	i = j = 0;
-	while (s1[i] != '\0')
-	{
-		strDup[i] = s1[i];
-		i++;
-	}
-	while (j < n && s2[j] != '\0')
-	{
-		strDup[i] = s2[j];
-		i++, j++;
-	}
-	strDup[i] = '\0';
-	return (strDup);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+
+if (sign >= len2)
+{
+sign = len2;
+ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+}
+else
+ptr = malloc(sizeof(char) * (len1 + n + 1));
+if (ptr == NULL)
+return (NULL);
+for (count = 0; count < len1; count++)
+{
+ptr[count] = s1[count];
+}
+for (count1 = 0; count1 < sign; count1++)
+{
+ptr[count++] = s2[count1];
+}
+ptr[count++] = '\0';
+return (ptr);
 }
